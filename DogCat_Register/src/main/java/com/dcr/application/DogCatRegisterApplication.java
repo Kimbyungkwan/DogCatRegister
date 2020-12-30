46f7cc3e924928f4d6fae7f677a8aff9abb6be7c
kqudk <kqudk@DESKTOP-GGOAV3M>
2020-12-30 15:06:34 +0900

    First commit.

---
+++ DogCat_Register/src/main/java/com/dcr/application/DogCatRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.dcr.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogCatRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogCatRegisterApplication.class, args);
+	}
+
+}
---
+++ DogCat_Register/src/test/java/com/dcr/application/DogCatRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcr.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogCatRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
